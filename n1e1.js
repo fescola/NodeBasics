ee5a605afab534356271ea566e978b6ba7a7f2d0
Mauricio Olivares <m.olivares.r@gmail.com>
2021-10-25 12:59:46 +0200

    commit

---
+++ n1e1.js
@@ -0,0 +1,28 @@
+//Nivell 1 exercici 1
+var usuari = prompt("Introdueix Nom");
+function nomUsuari(usuari) {
+  console.log("El nom es:" + usuari);
+}
+nomUsuari(usuari);
+
+//Nivell 2 Excercici 1
+const nom = prompt("Introdueix el teu nom de pila");
+const cognom = prompt("Introdueix el teu cognom");
+
+console.log(`Hola ${nom} ${cognom} `);
+
+//Nivell 2 Excercici 2
+
+function retornValor() {
+  let valor = prompt("introdueix un valor");
+  console.log(`
+   Hola ${nom}  ${cognom}
+   El teu valor es ${valor}
+   Bona tarda`);
+}
+
+retornValor();
+
+//Nivell 3 Exercici 1
+var matriu = [];
+for (let i = 0; i < 10; i++) {}
